1a54098064e8866be8f0581a4b085d0a03ff96c2
sameer240704 <sameergupta79711@gmail.com>
2025-03-16 12:09:52 +0530

    Added guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::rng().random_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please enter your guess:");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            },
+        };
 
-    println!("Hello there!");
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Please enter a smaller number"),
+            Ordering::Equal => {
+                println!("Congratulations! You have guessed the correct number");
+                break;
+            },
+            Ordering::Less => println!("Please enter a bigger number"),
+        }
+    }
 }
